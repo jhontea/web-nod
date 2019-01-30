ed004f08042f06309e9b6888a7585b3a3a5f2038
jhontea <hafizhipb49@gmail.com>
2019-01-30 17:33:03 +0700

    add 404

--- index.js
+++ index.js
@@ -9,37 +9,55 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .get('/rsvp', (req, res) => res.send(showRsvp()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
 
-showTimes = () => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  return result;
-}
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-showRsvp = () => {
-  return "This is rsvp"
-}
+// route here
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404).render('404', {title: "Sorry, page not found", session: req.sessionbo});
+});
+
+exports = module.exports = app;
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .get('/times', (req, res) => res.send(showTimes()))
+//   .get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect()
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
+//   .get('/rsvp', (req, res) => res.send(showRsvp()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// showTimes = () => {
+//   let result = ''
+//   const times = process.env.TIMES || 5
+//   for (i = 0; i < times; i++) {
+//     result += i + ' '
+//   }
+//   return result;
+// }
+
+// showRsvp = () => {
+//   return "This is rsvp"
+// }
